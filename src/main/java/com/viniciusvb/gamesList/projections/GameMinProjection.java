9b19f2d4af44914fb2732be61f23d5d6ac272bee
viniciusvilasbs <vinyvilas@gmail.com>
2023-07-15 10:35:04 -0300

    Homologation ok, correction at sql query for PostgreSQL

--- src/main/java/com/viniciusvb/gamesList/projections/GameMinProjection.java
+++ src/main/java/com/viniciusvb/gamesList/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.viniciusvb.gamesList.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
